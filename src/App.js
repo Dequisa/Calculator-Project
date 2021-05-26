774e3ee4348994bf5c0ed75758b77f4df1a8cdae
Dequisa <Dequisas@gmail.com>
2021-05-26 15:42:01 -0400

    calcuation changes

--- src/App.js
+++ src/App.js
@@ -4,30 +4,59 @@ import DisplayResults from './Component/DisplayResults'
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      results: 'I am the result',
-      number: ''
+    this.state = {
+      input: '',
+      firstNumber: '',
+      calculation: '1',
+      operationHit: false,
     }
   }
 
-  clear=() =>{
-    this.setState({results: ''})
+  clear = () => {
+    this.setState({ input: '' })
   }
-   onClick=(e)=>{
-this.setState({results: e.target.name})
+  onChange = (e) => {
+    console.log('hi')
+    this.setState ({
+      results: parseInt(e.target.value) 
+    })
+  }
+  onClick = (e) => {
+    const { input } = this.state
+    this.state.operationHit === false ? this.setState({ input: [...this.state.input] + e.target.name}) : this.setState({input: '' });
+    if (e.target.name === "+") {
+      this.setState({ operationHit: true, firstNumber: parseInt(this.state.input), calculation: 'a' })
+    } else if (e.target.name === "-") {
+      this.setState({ operationHit: true, firstNumber: input })
+    } else if (e.target.name === "x") {
+      this.setState({ operationHit: true, firstNumber: input})
+    } else if (e.target.name === "%") {
+      this.setState({ operationHit: true, firstNumber: input })
+    } else if(e.target.name === '=') {
+      this.calculate()
+    }
+    // else if (this.state.operationHit === false) {
+    //   this.setState({ 
+    //     input: [...this.state.input] + e.target.name
+    //     });
+       
+    // }
+    console.log("input:" + this.state.input, "firstNumber:" + this.state.firstNumber, "cal:" + this.state.calculation, this.state.operationHit)
 
   }
-  calculate = () =>{
+
+  calculate = () => {
+
 
   }
   render() {
-    console.log(this.onClick)
+    console.log(this.state.firstNumber)
     return (
       <div className="App">
-        <DisplayResults results={this.state.results}/>
-        <KeyComponent onClick={this.onClick} calculate={this.calculate} clear={this.clear}/>
+        <DisplayResults results={this.state.input} />
+        <KeyComponent onClick={this.onClick} onChange={this.onChange} calculate={this.calculate} clear={this.clear} />
       </div>
     );
   }
--- src/Component/KeyComponent.js
+++ src/Component/KeyComponent.js
@@ -3,28 +3,28 @@ import React from 'react';
 
 
 const KeyComponent = (props) => {
-    const {onClick, calculate, clear }= this.props
+    const {onChange, onClick, calculate, clear }= props
   
 
     return(
         <div>
         <button name='AC' onClick={clear}>AC</button>
-        <button name= '1' onClick={onclick}>1</button>
-        <button name= '2' onClick={onclick}>2</button>
-        <button name= '3' onClick={onclick}>3</button>
-        <button name= '4' onClick={onclick}>4</button>
-        <button name= '5' onClick={onclick}>5</button>
-        <button name= '6' onClick={onclick}>6</button>
-        <button name= '7' onClick={onclick}>7</button>
-        <button name= '8' onClick={onclick}>8</button>
-        <button name= '9' onClick={onclick}>9</button>
-        <button name= '0' onClick={onclick}>0</button>
-        <button name= '%' onClick>%</button>
-        <button name= 'X' onClick>X</button>
-        <button name= '-' onClick>-</button>
-        <button name= '+' onClick>+</button>
+        <button name= '1' value='1' onClick={onClick}>1</button>
+        <button name= '2' value='2' onClick={onClick}>2</button>
+        <button name= '3' value='3' onClick={onClick}>3</button>
+        <button name= '4' onClick={onClick}>4</button>
+        <button name= '5' onClick={onClick}>5</button>
+        <button name= '6' onClick={onClick}>6</button>
+        <button name= '7' onClick={onClick}>7</button>
+        <button name= '8' onClick={onClick}>8</button>
+        <button name= '9' onClick={onClick}>9</button>
+        <button name= '0' onClick={onClick}>0</button>
+        <button name= '/' >%</button>
+        <button name= '*' >*</button>
+        <button name= '-' >-</button>
+        <button name= '' onClick={onClick}>+</button>
         <button name= '=' onClick={calculate}>=</button>
-        <button name= '.' onClick>.</button>
+        <button name= '.' >.</button>
         </div>
     )
 
